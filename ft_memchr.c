93c820312c5c1a6e06fa2da0d1c5634ab962b42e
Esmaeil Saleh <esaleh@c3a8c1.42berlin.de>
2025-05-26 15:55:25 +0200

    Auto-commit on save

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 10:56:12 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/26 15:53:04 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/05/26 15:55:25 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,12 @@ void	*ft_memchr(const void *s, int c, size_t n)
 int	main(void)
 {
 	char str[] = "Hello how are you doing?";
-	if (ft_memchr(str, '?', 29*sizeof(char)))
+	unsigned char c = ft_memchr(str, '?', 29*sizeof(char));
+	if (c)
 	{
 		printf("%s\n", "Congratulation!");
 		printf("%s\n", "We have the char!");
-		printf("%c\n", ft_memchr(str, '?', 29*sizeof(char)));
+		printf("%c\n", c);
 	}
 	else
 		printf("%s\n", "No, We don't have it!'");
