1185f2bcbdd82c2736de351d7971357233746603
Esmaeil Saleh <esaleh@c3a8c1.42berlin.de>
2025-05-26 16:29:14 +0200

    Auto-commit on save

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 10:56:12 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/26 16:28:50 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/05/26 16:29:14 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (*(unsigned char *)s == c)
+		if (*(unsigned char *)s == tmp)
 		{
 			printf("%s\n", "Congratulation!");
 			return ((unsigned char *)s);
