4cc086c598ff1783bd695d94353da0c92ea6d608
EsmaeilSaleh <e.saleh88@gmail.com>
2025-05-28 14:51:51 +0200

    Auto-commit on save

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 10:56:12 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/28 14:51:30 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/05/28 14:51:51 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t			i;
-	unsigned char	tmp;
+	size_t				i;
+	unsigned char		tmp;
 	const unsigned char	ptr;
 
 	ptr = (const unsigned char *)s;
