e255ddac894d90f80e86f4b0530ccccea330a730
EsmaeilSaleh <e.saleh88@gmail.com>
2025-05-28 13:54:46 +0200

    Auto-commit on save

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 00:27:35 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/28 13:50:27 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/05/28 13:54:46 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,11 @@ char	*ft_strchr(const char *s, int c)
 	}
 	if (*s == '\0' && c == '\0')
 		return ((char *)s);
+	return (0);
+}
+
+int	main(void)
+{
+	edit
 	return (0);
 }
