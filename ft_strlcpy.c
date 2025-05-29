06697bf36876ea2faaba031ef6aa11b0fb33d054
EsmaeilSaleh <e.saleh88@gmail.com>
2025-05-29 12:53:30 +0200

    Auto-commit on save

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,13 +6,13 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 12:39:44 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/29 12:52:52 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/05/29 12:53:30 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-int	ft_strlcpy(char *dst, const char *str, size_t size)
+int	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	unsigned int	i;
 	unsigned int	j;
