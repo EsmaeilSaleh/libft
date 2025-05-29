b54b9991bd1dfda73ed6165623ebc484797aa037
EsmaeilSaleh <e.saleh88@gmail.com>
2025-05-29 13:19:38 +0200

    Auto-commit on save

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 11:05:13 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/29 13:19:16 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/05/29 13:19:38 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,6 @@
 # include <string.h>
 # include <stddef.h>
 
-void			*ft_memchr(const void *s, int c, size_t n);
-void			*ft_memset(void *s, int c, size_t n);
-void			*ft_memcpy(void *dest, void *src, size_t n);
-void			*ft_memmove(void *dest, void *src, size_t n);
-void			ft_bzero(void *s, size_t n);
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
 int			ft_isalnum(int c);
@@ -34,6 +29,11 @@ int			ft_strlcpy(char *dst, const char *src, size_t size);
 char			*ft_strchr(const char *s, int c);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
+void			*ft_memchr(const void *s, int c, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
+void			*ft_memcpy(void *dest, void *src, size_t n);
+void			*ft_memmove(void *dest, void *src, size_t n);
+void			ft_bzero(void *s, size_t n);
 unsigned int		ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
