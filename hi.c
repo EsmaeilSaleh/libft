1997534389d27c3472de49adc6215786c99ae75e
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-05-30 19:53:25 +0200

    update

--- hi.c
+++ hi.c
@@ -4,6 +4,5 @@ int	main(void) {
 	printf("%s\n", "Hello World!");
 	printf("%s\n", "This is a test to check git push");
 	printf("%s\n", "Working from home is better!");
-	printf("%s\n", "The change has done!");
 	return (0);
 }
