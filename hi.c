7af9083415c2c1f257d35041527f6ec8d101c891
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2025-06-01 08:07:56 +0200

    update

--- hi.c
+++ hi.c
@@ -2,6 +2,7 @@
 
 int	main(void) {
 	printf("%s\n", "Hello World!");
+	printf("%s\n", "-------------");
 	printf("%s\n", "Working from home is better!");
 	printf("%s\n", "How? Your Keyboard sucks!");
 	return (0);
