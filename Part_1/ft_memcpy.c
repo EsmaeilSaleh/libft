e48d7ad18d9ba89af09eda4ecab2ba804554529c
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 13:11:09 +0200

    Auto-commit on save

--- Part_1/ft_memcpy.c
+++ Part_1/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 09:20:48 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 13:10:51 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 13:11:09 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	size_t				i;
 	unsigned char		*d;
 	const unsigned char	*s;
-if (!dest && !src)
-	return (NULL);
+	if (!dest && !src)
+		return (NULL);
 
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
@@ -36,16 +36,16 @@ if (!dest && !src)
 
 void	*ft_memcpy(void *dest, void *src, size_t n)
 {
-	size_t	i;
+size_t	i;
 
-	i = 0;
-	while (i < n)
-	{
-		*(char *)dest = *(char *)src;
-		dest++;
-		src++;
-		i++;
-	}
-	return (dest - n);
-}
-*/
+i = 0;
+while (i < n)
+{
+ *(char *)dest = *(char *)src;
+ dest++;
+ src++;
+ i++;
+ }
+ return (dest - n);
+ }
+ */
