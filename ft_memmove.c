bf7813ed8c61adef7a7c8aabd600d0bf7ddb9bd9
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 11:46:49 +0200

    Auto-commit on save

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,40 +6,45 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 09:36:19 by esaleh            #+#    #+#             */
-/*   Updated: 2025/05/29 10:46:05 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 11:46:48 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include <stddef.h>
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void *ft_memmove(void *dest, const void *src, size_t n)
 {
-	size_t				i;
-	unsigned char		*d;
-	unsigned char		*orig_dest;
-	const unsigned char	*s;
+	unsigned char       *d;
+	const unsigned char *s;
+	size_t              i;
+
+	if (!dest && !src)
+	    return NULL;
 
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-	orig_dest = d;
-	if (!dest && !src && n > 0)
-		return (NULL);
-	if (d > s && d < s + n)
+
+	if (d < s)
 	{
-		while (n--)
-			d[n] = s[n];
+	    i = 0;
+	    while (i < n)
+	    {
+	        d[i] = s[i];
+	        i++;
+	    }
 	}
 	else
 	{
-		i = 0;
-		while (i < n)
-		{
-			d[i] = s[i];
-			i++;
-		}
+	    i = n;
+	    while (i > 0)
+	    {
+	        i--;
+	        d[i] = s[i];
+	    }
 	}
-	return ((void *)orig_dest);
+
+	return dest;
 }
+
 /*
 
 #include <stdio.h>
