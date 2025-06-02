9628c4c5a6fbac142fbbd8c893dd20d9104c62da
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 13:32:49 +0200

    Auto-commit on save

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,13 +6,13 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 13:19:30 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 13:30:43 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 13:32:49 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t				i;
 	const unsigned char	*p1;
