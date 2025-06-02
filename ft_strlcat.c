a7f130cb3e2ef035d07cfd818f649859f0a34245
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 12:36:01 +0200

    Auto-commit on save

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,12 +6,12 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 13:09:06 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 12:35:40 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 12:36:00 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <string.h>
 
-unsigned int	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	dst_len;
 	size_t	src_len;
