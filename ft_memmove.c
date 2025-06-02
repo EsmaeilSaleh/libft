01961a488268f5ca4da308fb3742ed10b07109e2
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 11:52:02 +0200

    Auto-commit on save

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,23 +6,19 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 09:36:19 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 11:49:51 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 11:52:02 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stddef.h>
-
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned char	*d;
 	const unsigned char	*s;
 	size_t	i;
-
 	if (!dest && !src)
 		return (NULL);
-
 	d = (unsigned char *)dest;
 	s = (const unsigned char *)src;
-
 	if (d < s)
 	{
 		i = 0;
@@ -41,19 +37,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 			d[i] = s[i];
 		}
 	}
-
 	return dest;
 }
-
 /*
-
 #include <stdio.h>
-
 void	*ft_memmove(void *dest, void *src, size_t n)
 {
 	size_t	i;
 	size_t	arr[50];
-
 	i = 0;
 	while (i < n)
 	{
