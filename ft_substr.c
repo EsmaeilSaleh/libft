1a6d2c38bc0893edf8657cb25f4797625da73088
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 14:13:07 +0200

    Auto-commit on save

---
+++ ft_substr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 14:11:27 by esaleh            #+#    #+#             */
+/*   Updated: 2025/06/02 14:13:07 by esaleh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"  // Make sure ft_strlen and ft_strlcpy (or ft_memcpy) are declared here
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	str_len;
+	size_t	substr_len;
+	char	*substr;
+
+	if (!s)
+		return (NULL);
+
+	str_len = ft_strlen(s);
+
+	// If start is past end of string, return empty string
+	if (start >= str_len)
+		return (ft_strdup(""));
+
+	// Calculate the actual substring length (can’t go past end of s)
+	substr_len = str_len - start;
+	if (substr_len > len)
+		substr_len = len;
+
+	substr = (char *)malloc(substr_len + 1);
+	if (!substr)
+		return (NULL);
+
+	// Copy substring + null terminator
+	ft_memcpy(substr, s + start, substr_len);
+	substr[substr_len] = '\0';
+
+	return (substr);
+}
