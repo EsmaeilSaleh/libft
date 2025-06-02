af6c16c9597a50c41c10397cecff4d8da62e0f91
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 12:33:16 +0200

    Auto-commit on save

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/22 11:05:13 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 12:32:21 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 12:33:16 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,6 @@ void	*ft_memset(void *s, int c, size_t n);
 void	*ft_memcpy(void *dest, void *src, size_t n);
 void	*ft_memmove(void *dest, void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
 
 #endif
