f6623b25e97f4ec364f2fd9ee3eb8c5313e5f26e
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 14:51:34 +0200

    Auto-commit on save

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 14:39:54 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 14:51:13 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 14:51:34 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	char		*new_str;
-	size_t		len;
-	unsigned int		i;
+	size_t			len;
+	unsigned int	i;
 
 	if (!s || !f)
 		return (NULL);
