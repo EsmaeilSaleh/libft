64032c4cfaa32e857cb1c99464b494aa8249d897
EsmaeilSaleh <e.saleh88@gmail.com>
2025-06-02 14:47:31 +0200

    Auto-commit on save

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,31 +6,31 @@
 /*   By: esaleh <esaleh@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 14:39:54 by esaleh            #+#    #+#             */
-/*   Updated: 2025/06/02 14:45:25 by esaleh           ###   ########.fr       */
+/*   Updated: 2025/06/02 14:47:31 by esaleh           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
-char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-        unsigned int            i;
-        char            *new_str;
-        size_t  len;
+	unsigned int            i;
+	char            *new_str;
+	size_t  len;
 
-        if (!s || !f)
-                return (NULL);
-        len = ft_strlen(s);
-        new_str = (char *)malloc(sizeof(char) * (len + 1));
-        if (!new_str)
-                return (NULL);
-        i = 0;
-        while (i < len)
-        {
-                new_str[i] = f(i, s[i]);
-                i++;
-        }
-        new_str[i] = '\0';
-        return (new_str);
+	if (!s || !f)
+	        return (NULL);
+	len = ft_strlen(s);
+	new_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!new_str)
+	        return (NULL);
+	i = 0;
+	while (i < len)
+	{
+	        new_str[i] = f(i, s[i]);
+	        i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
